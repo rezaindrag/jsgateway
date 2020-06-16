6bfeb79a8e9d5626d6c558e3ef3464e342b2b258
Reza Indra <dev.rezaindra@gmail.com>
2020-06-16 12:15:50 +0700

    docs: add comments

--- src/app.js
+++ src/app.js
@@ -13,6 +13,7 @@ function init(server, recipe) {
   for (let endpoint of recipe.endpoints) {
     const handler = async (req, res) => {
       try {
+        // This is used if your API requires authorization.
         await authorize(req.header("Authorization"));
       } catch (e) {
         return res.status(401).json({ message: e.message });
@@ -20,6 +21,7 @@ function init(server, recipe) {
 
       let statusCode = 0;
 
+      // Send the request URL from config.json.
       const proxy = req.pipe(request({
         uri: endpoint.service + req.url,
         qs: req.query,
@@ -30,6 +32,7 @@ function init(server, recipe) {
         statusCode = resp.statusCode;
       });
 
+      // Get the response and send to client.
       const originalString = await getStream(proxy);
 
       const data = JSON.parse(originalString);
